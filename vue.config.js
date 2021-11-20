be03075f72f51ff872b2d100e91b4ace992e233f
ravenera0317 <r6090408@gmail.com>
2021-11-20 15:23:24 +0800

    add deploy files

--- vue.config.js
+++ vue.config.js
@@ -6,8 +6,9 @@ module.exports = {
           @import '@/assets/styles/reset.scss';
           @import '@/assets/styles/color.scss';
           @import '@/assets/styles/mixin.scss';
-        `
-      }
-    }
-  }
+        `,
+      },
+    },
+  },
+  publicPath: process.env.NODE_ENV === 'production' ? '/alpha-shop-vue/' : '/',
 }
